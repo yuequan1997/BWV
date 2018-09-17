6786a4aa10135d8ff2d5d91bbf81efab0e69ac1b
yuequan <yuequan1997@gmail.com>
2018-09-18 00:47:02 +0800

    refactor: Add Comment

--- bwv-gui/src/main/java/org/yuequan/bwv/gui/view/ViewLoader.java
+++ bwv-gui/src/main/java/org/yuequan/bwv/gui/view/ViewLoader.java
@@ -2,7 +2,17 @@ package org.yuequan.bwv.gui.view;
 
 import javafx.fxml.FXMLLoader;
 
+/**
+ * Load Current Catalog XML
+ * @author yuequan
+ */
 public class ViewLoader {
+    /**
+     * Load fxml loader.
+     *
+     * @param fxmlPath the fxml path
+     * @return the fxml loader
+     */
     public static FXMLLoader load(String fxmlPath) {
         FXMLLoader loader = new FXMLLoader(ViewLoader.class.getResource(fxmlPath));
         return loader;
