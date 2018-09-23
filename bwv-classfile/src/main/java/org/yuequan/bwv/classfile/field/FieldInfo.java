0c7ea721742abe5263e0b185313646d233ad16b0
yuequan <yuequan1997@gmail.com>
2018-09-23 22:02:42 +0800

    feat: FieldInfo Add Get Methods

--- bwv-classfile/src/main/java/org/yuequan/bwv/classfile/field/FieldInfo.java
+++ bwv-classfile/src/main/java/org/yuequan/bwv/classfile/field/FieldInfo.java
@@ -21,4 +21,24 @@ public class FieldInfo {
             attributeInfos[i] = new AttributeInfo(fileReader);
         }
     }
+
+    public U2 getDescriptorIndex() {
+        return descriptorIndex;
+    }
+
+    public U2 getNameIndex() {
+        return nameIndex;
+    }
+
+    public AttributeInfo[] getAttributeInfos() {
+        return attributeInfos;
+    }
+
+    public U2 getAccessFlags() {
+        return accessFlags;
+    }
+
+    public U2 getAttributeCount() {
+        return attributeCount;
+    }
 }
